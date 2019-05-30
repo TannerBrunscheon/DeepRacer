7ca046458ea05584b1f42287e5d703a51f2ef68b
Tanner Brunscheon <tannerbrunscheon@gmail.com>
2019-05-30 11:54:49 -0500

    Update reward_function.py

--- reward_function.py
+++ reward_function.py
@@ -3,37 +3,25 @@ def reward_function(params):
     '''
     @on_track (boolean) :: The vehicle is off-track if the front of the vehicle is outside of the white
     lines
-
     @x (float range: [0, 1]) :: Fraction of where the car is along the x-axis. 1 indicates
     max 'x' value in the coordinate system.
-
     @y (float range: [0, 1]) :: Fraction of where the car is along the y-axis. 1 indicates
     max 'y' value in the coordinate system.
-
     @distance_from_center (float [0, track_width/2]) :: Displacement from the center line of the track
     as defined by way points
-
     @heading (float: [-180, 180]) :: yaw of the car with respect to the car's x-axis in
     radians (-180 degrees, 180 degrees)
-
     @progress (float: [0,1]) :: % of track complete
-
     @steps (int) :: numbers of steps completed. One step is one move by the car
-
     @speed :: (float) 0 to c In m/s
-
     @steering_angle :: (float) -30 to 30 (-30 is right, 30 is left)
-
     @track_width (float) :: width of the track (> 0)
-
     @waypoints (ordered list) :: list of waypoint in order; each waypoint is a set of coordinates
     (x,y,yaw) that define a turning point. Defines center.
-
     @closest_waypoints (int,int) :: index of the closest waypoint (0-indexed) given the car's x,y
     position as measured by the eucliedean distance
     
     @is_left_of_center (bool) :: is left of center
-
     @@output: @reward (float [-1e5, 1e5])
     '''
     on_track = params['all_wheels_on_track']
@@ -41,7 +29,7 @@ def reward_function(params):
     y = params["y"]
     distance_from_center = params["distance_from_center"]
     heading = params["heading"]
-    progress = params["progress"]/100
+    progress = params["progress"]
     steps = params["steps"]
     speed = params["speed"]
     steering = params["steering_angle"] /30
@@ -78,16 +66,17 @@ def reward_function(params):
 
     # Set Base Reward
     if not on_track: # Fail them if off Track
-        reward = REWARD_MIN
-        return reward
+        return REWARD_MIN
     elif progress == 1:
+        #the lap is complete.  if we use more steps than TARGET_STEPS, the reward is lower
+        return REWARD_MAX * TARGET_STEPS / steps
+    elif steps == 0:
+        #assuming that the car starts on a straight track, set the base reward to be the maximum
+        #we will adjust things later on
         reward = REWARD_MAX
-        return reward
-    elif progress < .1:
-        reward = REWARD_MAX
-    else:        # we want the vehicle to continue making progress
-        reward = REWARD_MAX * max(progress,.1)
-    
+    elif steps > 0:        # we want the vehicle to continue making progress
+        reward = REWARD_MAX * max(progress, 0.4)
+        
     #Check is Turning
     correction= 0
     next_next_point = waypoints[min(closest_waypoints[1]+1, len(waypoints)-1)]
@@ -111,40 +100,34 @@ def reward_function(params):
     # On straight
     ##########
     if not correction:
-        if speed < SPEED_MAX*.8:
-            reward *= (.125*speed)/SPEED_MAX
-        else:
-            reward *= .08*(16**(speed/SPEED_MAX)) 
-            
+        if speed != SPEED_MAX:
+            reward *= (speed/SPEED_MAX)**3
         if abs(steering) >.1:
-            reward *= (1/(abs(steering)+1))**10;
-        if is_left_of_center:
-            if (distance_from_center >0 and distance_from_center< track_width/4):
-                reward *= 1.4
-            else:
-                reward *= .9
+            reward *= (1/(abs(steering)+1))**20;
+        if distance_from_center < track_width/4:
+            #the position is good, so bump the reward up 30%
+            reward = reward * 1.3
+        elif distance_from_center < track_width/3:
+            #getting closer to the edge, so only increase the reward a bit
+            reward = reward * 1.10
+        elif distance_from_center < track_width/2:
+            #getting close to the edge, so decrease the reward a bit
+            reward = reward * 0.9
         else:
-            if (distance_from_center >0 and distance_from_center <track_width/8):
-                reward *= 1.1
-            elif(distance_from_center>track_width/8 and distance_from_center<track_width*3/8):
-                pass
-            else:
-                reward *=.9
+            #too close to the edge?  really discourage this position
+            reward = reward * .01
                 
     ##########
     # Around Curve
     ##########
     else:
-#        if abs(steering) > .75 and abs(steering) > speed/(CURVING_SPEED_MAX*2):
-#            reward *= max((1 - (steering - speed/(CURVING_SPEED_MAX*2)),.01))
+        if abs(steering) > .75 and abs(steering) > speed/(CURVING_SPEED_MAX*2):
+            reward *= max((1 - (steering - speed/(CURVING_SPEED_MAX*2)),.01))
         # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radia
     
         # Cacluate the difference between the track direction and the heading direction of the car
         direction_diff = abs(track_direction - heading)
-        if direction_diff <= 3:
-            reward *= 1.1
-        else:
-            reward *= 1-(direction_diff**2/100)
+        reward *= 1-((direction_diff**2/10)/10)
 
 
     # make sure reward value returned is within the prescribed value range.
